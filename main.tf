7b5f7b7cd23b55f6b38072125b8763a4522de28c
learnwithcodev <85067305+learnwithcodev@users.noreply.github.com>
2023-01-18 22:51:04 +0530

    update vpc module

--- main.tf
+++ main.tf
@@ -23,6 +23,8 @@ module "vpc" {
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
+  enable_nat_gateway = true
+
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -49,7 +51,6 @@ module "blog_sg" {
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-
   egress_rules = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
